73588f92d131ad47f3cd2b17a9653e655481efb8
Aum Patel <aumpatel131@gmail.com>
2026-02-19 18:16:33 +0530

    complete build system and add opencv c++ cmakge

--- core/bindings.cpp
+++ core/bindings.cpp
@@ -1,10 +1,26 @@
 #include <pybind11/pybind11.h>
+#include <stdio.h>
+
+#include <opencv2/opencv.hpp>
 namespace py = pybind11;
 
 int add(int a, int b) { return a + b; }
+
+void show_image(const std::string& path) {
+  cv::Mat image = cv::imread(path, cv::IMREAD_COLOR);
+
+  if (image.empty()) {
+    throw std::runtime_error("No image data");
+  }
+
+  cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE);
+  cv::imshow("Display Image", image);
+  cv::waitKey(0);
+}
 
 PYBIND11_MODULE(image_processing, m) {
   m.doc() = "Image processing module";
 
   m.def("add", &add, "Add two integers");
+  m.def("show_image", &show_image, "Display image using OpenCV");
 }
--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,4 @@ sys.path.append(os.path.abspath("core/build"))
 import image_processing
 
 print(image_processing.add(2, 4))
+image_processing.show_image("img.png")
